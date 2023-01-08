e4279384ce71dbf0211839a9799e8b6d8221ac55
Nicholas Trampe <nicholas.trampe@gmail.com>
2023-01-08 09:05:04 +0100

    Removing masonry

--- js/plugins.js
+++ js/plugins.js
@@ -10,7 +10,6 @@
 
 // @codekit-prepend "/plugins/history.js"
 // @codekit-prepend "/plugins/imagesloaded.js"
-// @codekit-prepend "/plugins/masonry.js"
 // @codekit-prepend "/plugins/debounce.js"
 // @codekit-prepend "/plugins/fluidbox.js"
 // @codekit-prepend "/plugins/waypoints.js"
