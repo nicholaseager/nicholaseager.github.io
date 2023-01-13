0b4cfed27c217d86cfd9764cec5d5fc888b53419
Nicholas Trampe <nicholas.trampe@gmail.com>
2023-01-13 20:52:56 +0100

    Removing swiper module usage

--- js/board.js
+++ js/board.js
@@ -1,5 +1,3 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
-
 (function ($) {
 	'use strict';
 
@@ -154,6 +152,82 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.brow
 		} else {
 			mainVideo.append("<source type='video/mp4' src='" + desktopSrc + "' />");
 		}
+
+		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Swipers
+
+		const singleSlideSwiper = new Swiper('.swiperSingle', {
+			// Optional parameters
+			direction: 'horizontal',
+			loop: true,
+			autoHeight: true,
+			preloadImages: false,
+		
+			autoplay: {
+				delay: 5000,
+			},
+		
+			lazy: {
+				loadPrevNext: true,
+			},
+		});
+		
+		const dynamicSlideSwiper = new Swiper('.swiperDynamic', {
+			// Optional parameters
+			direction: 'horizontal',
+			loop: true,
+			preloadImages: false,
+		
+			autoplay: {
+				delay: 5000,
+			},
+		
+			lazy: {
+				loadPrevNext: true,
+			},
+			breakpoints: {
+				480: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				960: {
+					slidesPerView: 2,
+					spaceBetween: 20
+				},
+				1440: {
+					slidesPerView: 3,
+					spaceBetween: 30
+				}
+			}
+		});
+		
+		const collectionSwiper = new Swiper('.swiperCollection', {
+			// Optional parameters
+			direction: 'horizontal',
+			loop: false,
+			autoHeight: true,
+		
+			autoplay: {
+				delay: 5000,
+			},
+		
+			lazy: {
+				loadPrevNext: true,
+			},
+			breakpoints: {
+				480: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				960: {
+					slidesPerView: 2,
+					spaceBetween: 5
+				},
+				1440: {
+					slidesPerView: 3,
+					spaceBetween: 10
+				}
+			}
+		});
 
 		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Galleries
 
