2fa7fd7b946fd6351638e181a729f98476348b6b
Nicholas Trampe <nicholas.trampe@gmail.com>
2023-01-18 08:41:02 +0100

    Updating script to minify js automatically

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python prints_to_yaml.py && bundle exec jekyll serve
+rm js/board-min.js && uglifyjs js/board.js >> js/board-min.js
+rm js/plugins-min.js && uglifyjs js/plugins/*.js >> js/plugins-min.js
+python prints_to_yaml.py &&
+bundle exec jekyll serve
