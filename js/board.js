706ff41772381c33ea5f327bd5bf1cd5d3f96770
Nicholas Trampe <nicholas.trampe@gmail.com>
2023-06-25 08:59:13 +0700

    Checking for window existing first

--- js/board.js
+++ js/board.js
@@ -427,15 +427,19 @@
 				var id = $this.attr('id');
 				if (id && id.startsWith("lazyframe")) {
 					if (isScrolledIntoView($this, 50)) {
-						// Remove srcdoc containing background and load button
-						$this.removeAttr("srcdoc");
-
-						// Replace the iframe source with the intended source
 						// This method avoids an issue with the window history duplicates
-						(this.contentWindow || this.documentWindow).location.replace($this.attr('data-src'));
+						var window = (this.contentWindow || this.documentWindow);
 
-						// Remove the lazy id so this doesn't happen again
-						$this.attr('id', id.replace('lazyframe', ''));
+						if (window) {
+							// Remove srcdoc containing background and load button
+							$this.removeAttr("srcdoc");
+
+							// Replace the iframe source with the intended source
+							window.location.replace($this.attr('data-src'));
+
+							// Remove the lazy id so this doesn't happen again
+							$this.attr('id', id.replace('lazyframe', ''));
+						}
 					}
 				}
 			});
