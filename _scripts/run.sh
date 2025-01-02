8abc3d6faff607922c6833a0371d219869ad07ad
Nicholas Eager <nicholas@nicholaseager.com>
2025-01-02 15:34:59 +0900

    chore: removing unused python scripts

--- _scripts/run.sh
+++ _scripts/run.sh
@@ -8,17 +8,5 @@ echo -n '.'
 rm js/plugins-min.js && uglifyjs js/plugins/*.js >>js/plugins-min.js
 echo ' done!'
 
-# Generate Pages
-
-echo -n '📸 generating pages ...'
-python _scripts/photos_to_yaml.py
-echo -n '.'
-python _scripts/galleries_to_yaml.py
-echo -n '.'
-python _scripts/guides_to_yaml.py
-echo -n '.'
-python _scripts/generate_metadata.py
-echo ' done!'
-
 echo '📸 serving website ...'
 bundle exec jekyll serve
