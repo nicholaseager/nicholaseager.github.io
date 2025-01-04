6f0e621ef017a83e377f028b2cf3302640169b86
Kitsune Apps <kitsune.apps@gmail.com>
2025-01-04 09:41:44 +0900

    chore: adding tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap(), tailwind()]
 });
