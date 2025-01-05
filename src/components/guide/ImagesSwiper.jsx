5dd8b5df743baa9012d9eb2d3d3b3642c80dde74
Kitsune Apps <kitsune.apps@gmail.com>
2025-01-05 12:53:37 +0900

    refactor: rename swiper

---
+++ src/components/guide/ImagesSwiper.jsx
@@ -0,0 +1,19 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+
+import Image from "../image-kit/Image.tsx";
+
+const ImagesSwiper = ({ imagePaths }) => {
+  return (
+    <Swiper slidesPerView={1} spaceBetween={20} grabCursor={true}>
+      {imagePaths.map((path, index) => (
+        <SwiperSlide key={index} className="rounded-lg shadow-lg">
+          <Image path={path} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ImagesSwiper;
