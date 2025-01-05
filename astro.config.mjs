1e3126ac844c37e6631f61b9481a6dcade87cf88
Kitsune Apps <kitsune.apps@gmail.com>
2025-01-06 07:49:00 +0900

    feat: adding footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import sitemap from '@astrojs/sitemap';
-
-import tailwind from '@astrojs/tailwind';
-
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), sitemap(), tailwind(), mdx()]
+  integrations: [react(), sitemap(), tailwind(), mdx()],
+  experimental: {
+    svg: true,
+  },
 });
