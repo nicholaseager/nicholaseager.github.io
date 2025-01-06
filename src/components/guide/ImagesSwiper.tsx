c90569f1df4cc66f007d6e57b72b7af8ea3e485a
Kitsune Apps <kitsune.apps@gmail.com>
2025-01-06 10:28:21 +0900

    feat: allowing variable breakpoints on guide image swipers

--- src/components/guide/ImagesSwiper.tsx
+++ src/components/guide/ImagesSwiper.tsx
@@ -3,17 +3,50 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 import Image from "../image-kit/Image";
+import Card from "../ui/Card";
 
 interface ImagesSwiperProps {
   imagePaths: string[];
 }
 
 const ImagesSwiper: React.FC<ImagesSwiperProps> = ({ imagePaths }) => {
+  const getBreakpoints = () => {
+    const totalSlides = imagePaths.length;
+
+    if (totalSlides < 3) {
+      return {
+        320: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: Math.min(totalSlides, 2),
+        },
+        1024: {
+          slidesPerView: totalSlides,
+        },
+      };
+    }
+
+    return {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    };
+  };
+
   return (
-    <Swiper slidesPerView={1} spaceBetween={20} grabCursor={true}>
+    <Swiper breakpoints={getBreakpoints()} spaceBetween={20} grabCursor={true}>
       {imagePaths.map((path, index) => (
-        <SwiperSlide key={index} className="rounded-lg shadow-lg">
-          <Image path={path} />
+        <SwiperSlide key={index}>
+          <div className="rounded-lg shadow-lg overflow-hidden">
+            <Image path={path} />
+          </div>
         </SwiperSlide>
       ))}
     </Swiper>
