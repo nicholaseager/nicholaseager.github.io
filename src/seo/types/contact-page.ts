e053322cc3afcee0f7926552398ccb88770317de
Kitsune Apps <kitsune.apps@gmail.com>
2025-01-09 08:27:39 +0900

    feat: adding contact page seo

---
+++ src/seo/types/contact-page.ts
@@ -0,0 +1,37 @@
+import type { ContactPage } from "schema-dts";
+import { organizationFrom } from "./organization";
+
+interface ContactPageSchemaProps {
+  name: string;
+  description: string;
+  url: URL;
+}
+
+export function contactPageFrom({
+  name,
+  description,
+  url,
+}: ContactPageSchemaProps): ContactPage {
+  return {
+    "@type": "ContactPage",
+    name,
+    description,
+    url: url.toString(),
+    publisher: organizationFrom({ url: url }),
+    isPartOf: {
+      "@type": "WebPage",
+      "@id": `${url}#webpage`,
+    },
+    potentialAction: {
+      "@type": "InteractAction",
+      target: {
+        "@type": "EntryPoint",
+        urlTemplate: url.toString(),
+        actionPlatform: [
+          "http://schema.org/DesktopWebPlatform",
+          "http://schema.org/MobileWebPlatform",
+        ],
+      },
+    },
+  };
+}
