b339afe4f92bf71a00a3cc1b6637ae977e7201d8
Kitsune Apps <kitsune.apps@gmail.com>
2025-01-11 12:24:52 +0700

    refactor: using base swiper for quotes

--- src/components/testimonials/TestimonialSwiper.tsx
+++ src/components/testimonials/TestimonialSwiper.tsx
@@ -2,22 +2,30 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 
-import TestimonialCard from "./TestimonialCard.tsx";
 import testimonials from "../../data/testimonials.json";
+import BaseSwiper from "../ui/BaseSwiper.tsx";
+import TestimonialCard from "./TestimonialCard.tsx";
 
 const TestimonialSwiper: React.FC = () => {
   return (
-    <Swiper slidesPerView={1} spaceBetween={40} grabCursor={true}>
-      {testimonials.map((item, index) => (
-        <SwiperSlide key={index}>
-          <TestimonialCard
-            quote={item.quote}
-            author={item.author}
-            url={item.url}
-          />
-        </SwiperSlide>
+    <BaseSwiper
+      breakpoints={{
+        xs: 1,
+        sm: 1,
+        md: 2,
+        lg: 2,
+        xl: 2,
+        "2xl": 3,
+      }}
+    >
+      {testimonials.map((item) => (
+        <TestimonialCard
+          quote={item.quote}
+          author={item.author}
+          url={item.url}
+        />
       ))}
-    </Swiper>
+    </BaseSwiper>
   );
 };
 
