bd5973c0d344a296c3ac398611c23b26c5a377ff
Kitsune Apps <kitsune.apps@gmail.com>
2025-01-11 13:23:04 +0700

    feat: using a lower res video on mobile

--- src/components/image-kit/Video.tsx
+++ src/components/image-kit/Video.tsx
@@ -1,17 +1,22 @@
 interface VideoProps {
   path: string;
+  lowResPath?: string;
   className?: string;
 }
 
 const CDN_BASE_URL = import.meta.env.PUBLIC_CDN_BASE_URL;
 
-const Video = ({ path, className }: VideoProps) => {
-  const parts = path.split("/");
-  const alt = parts[parts.length - 1].replace(/-/g, " ");
-
+const Video = ({ path, lowResPath, className }: VideoProps) => {
   return (
     <video autoPlay loop muted className={`w-full ${className || ""}`}>
-      <source src={CDN_BASE_URL + path} type="video/mp4" />
+      <source
+        media="(min-width: 768px)"
+        src={CDN_BASE_URL + path}
+        type="video/mp4"
+      />
+      {lowResPath && (
+        <source src={CDN_BASE_URL + lowResPath} type="video/mp4" />
+      )}
     </video>
   );
 };
