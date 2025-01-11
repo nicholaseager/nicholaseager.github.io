99fae2f992a78b34df49c8954872c311fb922127
Kitsune Apps <kitsune.apps@gmail.com>
2025-01-11 10:24:01 +0700

    fix: removing swiper clipping

--- src/components/ui/BaseSwiper.tsx
+++ src/components/ui/BaseSwiper.tsx
@@ -17,26 +17,31 @@ interface BaseSwiperProps {
 
 const BaseSwiper: React.FC<BaseSwiperProps> = ({ items, urlPrefix }) => {
   return (
-    <Swiper
-      breakpoints={{
-        320: { slidesPerView: 1 },
-        768: { slidesPerView: 2 },
-        1024: { slidesPerView: 3 },
-      }}
-      spaceBetween={20}
-      grabCursor={true}
-    >
-      {items.map((item, index) => (
-        <SwiperSlide key={index}>
-          <ArticleCard
-            href={`${urlPrefix}/${item.id}/`}
-            imagePath={item.image}
-            title={item.title}
-            description={item.description}
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="overflow-hidden">
+      <div className="p-4">
+        <Swiper
+          breakpoints={{
+            320: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+          spaceBetween={30}
+          grabCursor={true}
+          style={{ overflow: "visible" }}
+        >
+          {items.map((item, index) => (
+            <SwiperSlide key={index}>
+              <ArticleCard
+                href={`${urlPrefix}/${item.id}/`}
+                imagePath={item.image}
+                title={item.title}
+                description={item.description}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
   );
 };
 
