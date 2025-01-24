826eac3d59c7447ec0b09bea48f97c6af8506ead
Kitsune Apps <kitsune.apps@gmail.com>
2025-01-24 06:00:00 -0600

    fix: using slots to add header elements

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -35,37 +35,35 @@ export default function NavBar({ pathname, style }: NavBarProps) {
     links.find((link) => link.href === pathname)?.name || "Menu";
 
   return (
-    <header role="banner">
-      <nav className={`${backgroundColor} p-4 ${positioning} z-50 w-full`}>
-        <div className="mx-auto flex items-center justify-between">
-          <div className={`flex-shrink-0 ${textColor} text-2xl`}>
-            <h1>
-              <a href="/">Nicholas Eager</a>
-            </h1>
-          </div>
-
-          <div className="hidden md:flex space-x-4">
-            {links.map((link) => (
-              <a
-                key={link.href}
-                href={link.href}
-                className={`${textColor} px-3 py-2 rounded-md text-lg font-medium ${
-                  pathname === link.href ? selectedColor : hoverColor
-                }`}
-              >
-                {link.name}
-              </a>
-            ))}
-          </div>
-
-          <Dropdown
-            className="md:hidden"
-            items={dropdownItems}
-            activeItemId={pathname}
-            currentLabel={currentPage}
-          />
+    <nav className={`${backgroundColor} p-4 ${positioning} z-50 w-full`}>
+      <div className="mx-auto flex items-center justify-between">
+        <div className={`flex-shrink-0 ${textColor} text-2xl`}>
+          <h1>
+            <a href="/">Nicholas Eager</a>
+          </h1>
         </div>
-      </nav>
-    </header>
+
+        <div className="hidden md:flex space-x-4">
+          {links.map((link) => (
+            <a
+              key={link.href}
+              href={link.href}
+              className={`${textColor} px-3 py-2 rounded-md text-lg font-medium ${
+                pathname === link.href ? selectedColor : hoverColor
+              }`}
+            >
+              {link.name}
+            </a>
+          ))}
+        </div>
+
+        <Dropdown
+          className="md:hidden"
+          items={dropdownItems}
+          activeItemId={pathname}
+          currentLabel={currentPage}
+        />
+      </div>
+    </nav>
   );
 }
